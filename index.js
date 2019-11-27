281bc10cdfc9ff1c4531326587b2fc3a0e8646e2
Masami Suzuki <msuzuki@heroku.com>
2019-11-27 18:30:27 +0900

    add loader.io

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get('/api/inc', (req,res) => {
    res.send(`${ncount++}`);
 });
 
+app.get('loaderio-0d1ea6833e5eb9b16892d568f3d73932',req,res) => {
+    res.send('loaderio-0d1ea6833e5eb9b16892d568f3d73932');
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
